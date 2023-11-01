e0e4b2284f6da39c14a5e031e7b8fb3968b60c01
Shubham Chandgude <shubhamchandgude1998@gmail.com>
2023-11-01 12:07:13 +0530

    Login page first push

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Login.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+const Login = () => {
+    return (
+        <>
+            <section className="main-section-login triple-cloud">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 text-center">
+                            <div className="login-wrap">
+                                <div className="form-login">
+                                    <div className="col-sm-12 text-center">
+                                        <img width="443" src="https://rmsstaging.hatchearlychildhood.com/images/insight/insights_logo.png" alt="" className="img-fluid" />
+                                    </div>
+                                    <form autoComplete='off' action="/" className="form-horizontal" id='login-form' method='post'>
+                                        <div className="login-message">
+                                            <h3>Please Log In</h3>
+                                        </div>
+                                        <div className="form-group mt-3 no-gutters">
+                                            <div className="col-sm-12 text-left">
+                                                <input type='hidden' name='LoginForm' value='/' />
+                                                <label for className='control-label pl-2'>Email Address</label>
+                                                <div className="input-block">
+                                                    <input autoComplete='off' type="email" id="LoginForm_username" placeholder className='form-control is-required-email' maxLength='50' tabIndex='1' />
+                                                    <p className="errorlabel mb-0"></p>
+                                                    <div className="errorMessage" id='LoginForm' style={{ display: 'none' }}></div>
+                                                    <p></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="form-group no-gutters">
+                                            <div className="col-sm-12 text-left">
+                                                <label for className='control-label pl-2'>Password</label>
+                                                <div className="input-block">
+                                                    <input autoComplete='off' type="password" id="LoginForm_password" className='form-control' placeholder maxLength='50' tabIndex='2' name='LoginForm' />
+                                                    <a className='pass-toggle toggled' id='LoginForm_show' href="#" title='Show Password'></a>
+                                                </div>
+                                                <p className="errorlabel mb-0"></p>
+                                                <p className="errorlabel mb-0"></p>
+                                                <div class="errorMessage" id="LoginForm_password_em_" style={{ display: "none" }}></div>
+                                                <p></p>
+                                                <p className="errorlabel mb-0" id="info_alert" style={{ display: "none" }}></p>
+                                            </div>
+                                        </div>
+                                        <div className="mt-30">
+                                            <button className="btn btn-block btn-login" id="setPassBtn" type='submit' tabIndex='3'>Log In</button>
+                                            <div class="login-extra-links mt-30">
+                                                <a href="https://rmsstaging.hatchearlychildhood.com/site/forgotpassword?token=mlsipqtnkhejcbforagd">Forget your password?</a>
+                                                |
+                                                <a target="_blank" href="https://support.hatchearlylearning.com/hc/en-us/articles/4526935151380">Login Help </a>
+                                                |
+                                                <a target="_blank" href="https://support.hatchearlylearning.com/hc/en-us/articles/360057006572">How do I get an account?</a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div class="sign-with-classlink mt-5 mt-md-4 mt-lg-5">
+                                <a href="https://launchpad.classlink.com/oauth2/v2/auth?scope=full,profile&red…&client_id=c16021754185055cafb4470494668704b25cbcc7c466&response_type=code" target="_blank">
+                                    <img width='300px' src="https://rmsstaging.hatchearlychildhood.com/images/insight/sign-with-classlink.png" class="img-fluid" alt></img>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* //Footer */}
+                <div class="footer full-width-footer">
+                    <ul class="footer-links">
+                        <li>
+                            ©
+                            <span>2023 Hatch, Inc.&emsp;|</span>
+                        </li>
+                        <li>
+                            <span>
+                                <a href="https://www.hatchearlylearning.com" target="_blank" rel="noopener"> HatchEarlyLearning.com</a>
+                                &emsp;|
+                            </span>
+                        </li>
+                        <li>
+                            <i class="fas fa-phone"></i>
+                            &nbsp;&nbsp;&nbsp;
+                            <span>1.800.624.7968 option 4</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="help-button">
+                    <button className='btn btn-primary'>
+                        <i class="fa-regular fa-circle-question"></i>
+                        Help
+                    </button>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Login;
